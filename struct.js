fc1d7331a569248d18f20d582d26aec098570587
DOSexample <saytanito@gmail.com>
2017-04-04 11:44:00 +0700

    Add files via upload

--- struct.js
+++ struct.js
@@ -1,16 +1,35 @@
 var config = require('./config');
-this._ = require('c-struct');
+var _ = require('c-struct');
 
-this.AVATAR_INFO = new this._.Schema({
-  aVisibleState: this._.type.uint32,
-  aSpecialState: this._.type.uint32,
-  aPlayTime1: this._.type.uint32,
-  aPlayTime1: this._.type.uint32,
-  aPlayTime2: this._.type.uint32, 
-  aKillOtherTribe: this._.type.uint32,
-  aName: this._.type.string( config.MAX_AVATAR_NAME_LENGTH ),
-  aBuff01: this._.type.string( 3 )
+this.AVATAR_INFO = new _.Schema({
+	aVisibleState: _.type.uint32,
+	aSpecialState: _.type.uint32,
+	aPlayTime1: _.type.uint32,
+	aPlayTime2: _.type.uint32, 
+	aKillOtherTribe: _.type.uint32,
+	aName: _.type.string( config.MAX_AVATAR_NAME_LENGTH ),
+	aBuff01: _.type.string( 3 ),
+	aTribe: _.type.uint32,
+	aPreviousTribe: _.type.uint32,
+	aGender: _.type.uint32,
+	aHeadType: _.type.uint32,
+	aFaceType: _.type.uint32,
+	aLevel1: _.type.uint32,
+	aLevel2: _.type.uint32,
+	aGeneralExperience1: _.type.uint32,
+	aGeneralExperience2: _.type.uint32,
+	aVit: _.type.uint32,
+	aStr: _.type.uint32,
+	aInt: _.type.uint32,
+	aDex: _.type.uint32
 });
-// register to cache
-this._.register('AVATAR_INFO', this.AVATAR_INFO);
+_.register('AVATAR_INFO', this.AVATAR_INFO);
+this.pack = function ( AVATAR_INFO )
+{
+	return _.packSync( 'AVATAR_INFO', AVATAR_INFO );
+}
+this.unpack = function ( AVATAR_INFO )
+{
+	return _.unpackSync( 'AVATAR_INFO', AVATAR_INFO );
+}
 module.exports = this;
