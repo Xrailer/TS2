67325931bbccfbfa3671df154ac2943acb71bb7f
DOSexample <saytanito@gmail.com>
2017-04-06 08:10:01 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -5,15 +5,19 @@ global.LOGIN_IP = '127.0.0.1';
 global.LOGIN_PO = 11091;
 global.MAX_USER_FOR_LOGIN = 1000;
 
-global.MY_HOST = '127.0.0.1';
-global.MY_PORT = 3306;
-global.MY_USER = 'root';
-global.MY_PASS = '';
-global.MY_DB = 'game';
-global.MY_TB01 = 'DefineMaxUserNum';
-global.MY_TB02 = 'MemberInfo';
-global.MY_TB03 = 'AvatarInfo';
-global.MY_TB04 = 'MasterInfo';
+global.ZONE_IP = '127.0.0.1';
+global.ZONE_PO = 20001;
+global.MAX_USER_FOR_ZONE = 1000;
+
+global.MY_HOST = '127.0.0.1';				//your mysql host
+global.MY_PORT = 3306;						//your mysql port
+global.MY_USER = 'root';					//your mysql user
+global.MY_PASS = '';						//your mysql password
+global.MY_DB = 'game';						//your mysql database
+global.MY_TB01 = 'DefineMaxUserNum';		//mysql table
+global.MY_TB02 = 'MemberInfo';				//mysql table
+global.MY_TB03 = 'AvatarInfo';				//mysql table
+global.MY_TB04 = 'MasterInfo';				//mysql table
 
 global.MAX_USER_ID_LENGTH 				=	101;
 global.MAX_USER_PASSWORD_LENGTH			=	101;
@@ -33,7 +37,11 @@ global.MAX_TRADE_SLOT_NUM				=	8;
 global.MAX_HOT_KEY_NUM					=	14;
 global.MAX_FRIEND_NUM					=	10;
 
+
+global.MAX_AVATAR_EFFECT_SORT_NUM		=	29;
 global.SIZE_OF_AVATAR_INFO				=	7896;
+global.SIZE_OF_ACTION_INFO				=	72;
+global.SIZE_OF_OBJECT_FOR_AVATAR		=	556;
 //CLIENT -> LOGIN;
 global.P_LOGIN_SEND						=	11;
 global.S_LOGIN_SEND						=	13 + MAX_USER_ID_LENGTH + MAX_USER_PASSWORD_LENGTH;
@@ -49,12 +57,16 @@ global.P_CREATE_AVATAR_SEND 			=	17;
 global.S_CREATE_AVATAR_SEND				=	13 + SIZE_OF_AVATAR_INFO;
 global.P_DELETE_AVATAR_SEND				=	18;
 global.S_DELETE_AVATAR_SEND				=	21;
+global.P_DEMAND_ZONE_SERVER_INFO_1_SEND	=	22;
+global.S_DEMAND_ZONE_SERVER_INFO_1_SEND	=	13;
+global.P_FAIL_MOVE_ZONE_1_SEND			=	23;
+global.S_FAIL_MOVE_ZONE_1_SEND			=	9;
 
 //LOGIN -> CLIENT;
 global.P_LOGIN_RECV						=	11;
 global.S_LOGIN_RECV						=	339;
-global.P_USER_AVATAR_INFO					=	12;
-global.S_USER_AVATAR_INFO					=	1 + SIZE_OF_AVATAR_INFO;
+global.P_USER_AVATAR_INFO				=	12;
+global.S_USER_AVATAR_INFO				=	1 + SIZE_OF_AVATAR_INFO;
 global.P_CREATE_MOUSE_PASSWORD_RECV		=	13;
 global.S_CREATE_MOUSE_PASSWORD_RECV		=	5 + MAX_MOUSE_PASSWORD_LENGTH;
 global.P_CHANGE_MOUSE_PASSWORD_RECV		=	14;
@@ -65,7 +77,27 @@ global.P_CREATE_AVATAR_RECV				=	17
 global.S_CREATE_AVATAR_RECV				=	5 + SIZE_OF_AVATAR_INFO;
 global.P_DELETE_AVATAR_RECV				=	18;
 global.S_DELETE_AVATAR_RECV				=	5;
-global.P_RECOMMAND_WORLD_SEND				=	24;
-global.S_RECOMMAND_WORLD_SEND				=	13;
+global.P_DEMAND_ZONE_SERVER_INFO_1_RECV	=	22;
+global.S_DEMAND_ZONE_SERVER_INFO_1_RECV	=	29;
+global.P_RECOMMAND_WORLD_SEND			=	24;
+global.S_RECOMMAND_WORLD_SEND			=	13;
+
+
+
+//CLIENT -> ZONE
+global.P_TEMP_REGISTER_SEND				=	11;
+global.S_TEMP_REGISTER_SEND				=	13 + MAX_USER_ID_LENGTH;
+global.P_REGISTER_AVATAR_SEND			=	12;
+global.S_REGISTER_AVATAR_SEND			=	9 + MAX_USER_ID_LENGTH + MAX_AVATAR_NAME_LENGTH + SIZE_OF_ACTION_INFO;
+
+//ZONE -> CLIENT
+global.P_TEMP_REGISTER_RECV				=	11;
+global.S_TEMP_REGISTER_RECV				=	5;
+global.P_REGISTER_AVATAR_RECV			=	12;
+global.S_REGISTER_AVATAR_RECV			=	1 + SIZE_OF_AVATAR_INFO + ( MAX_AVATAR_EFFECT_SORT_NUM * 8 );
+global.P_AVATAR_ACTION_RECV				=	15;
+global.S_AVATAR_ACTION_RECV				=	13 + SIZE_OF_OBJECT_FOR_AVATAR;
+
+
 
 module.exports = global;
--- struct.js
+++ struct.js
@@ -2,57 +2,124 @@ const ezstruct = require('ezstruct');
 ezstruct.setStringEncoding('utf-8');
 var context = ezstruct `
 typedef struct {
-	int aVisibleState;
-	int aSpecialState;
-	int aPlayTime1;
-	int aPlayTime2;
-	int aKillOtherTribe;
-	char aName[13];
-	char aBuff0[3];
-	int aTribe;
-	int aPreviousTribe;
-	int aGender;
-	int aHeadType;
-	int aFaceType;
-	int aLevel1;
-	int aLevel2;
-	int aGeneralExperience1;
-	int aGeneralExperience2;
-	int aVitality;
-	int aStrength;
-	int aKi;
-	int aWisdom;
-	int aEatLifePotion;
-	int aEatManaPotion;
-	int aStateBonusPoint;
-	int aSkillPoint;
-	int aEquip[13][4];
-	int aExpandInventoryDate;
-	int aMoney;
-	int aInventory[2][64][6];
-	int aTradeMoney;
-	int aTrade[8][4];
-	int aExpandStoreDate;
-	int aStoreMoney;
-	int aStoreItem[2][28][4];
-	int aSkill[40][2];
-	int aHotKey[3][14][3];
-	int aQuestInfo[5];
-	char aFriend[10][13];
-	char aTeacher[13];
-	char aStudent[13];
-	int aTeacherPoint;
-	char aGuildName[13];
-	int aGuildRole;
-	char aCallName[5];
-	int aGuildMarkNum;
-	int aGuildMarkEffect;
-	int aLogoutInfo[6];
-	char aFree1[264];
-	int uSaveMoney;
-	int uSaveItem[28][4];
-	char aFree2[1688];
+	int 	aVisibleState;
+	int 	aSpecialState;
+	int 	aPlayTime1;
+	int 	aPlayTime2;
+	int 	aKillOtherTribe;
+	char	aName[13];
+	char	aBuff0[3];
+	int 	aTribe;
+	int 	aPreviousTribe;
+	int 	aGender;
+	int 	aHeadType;
+	int 	aFaceType;
+	int 	aLevel1;
+	int 	aLevel2;
+	int 	aGeneralExperience1;
+	int 	aGeneralExperience2;
+	int 	aVitality;
+	int 	aStrength;
+	int 	aKi;
+	int 	aWisdom;
+	int 	aEatLifePotion;
+	int 	aEatManaPotion;
+	int 	aStateBonusPoint;
+	int 	aSkillPoint;
+	int 	aEquip[13][4];
+	int 	aExpandInventoryDate;
+	int 	aMoney;
+	int 	aInventory[2][64][6];
+	int 	aTradeMoney;
+	int 	aTrade[8][4];
+	int 	aExpandStoreDate;
+	int 	aStoreMoney;
+	int 	aStoreItem[2][28][4];
+	int 	aSkill[40][2];
+	int 	aHotKey[3][14][3];
+	int 	aQuestInfo[5];
+	char	aFriend[10][13];
+	char	aTeacher[13];
+	char	aStudent[13];
+	int 	aTeacherPoint;
+	char	aGuildName[13];
+	int 	aGuildRole;
+	char	aCallName[5];
+	int 	aGuildMarkNum;
+	int 	aGuildMarkEffect;
+	int 	aLogoutInfo[6];
+	char	aFree1[264];
+	int 	uSaveMoney;
+	int 	uSaveItem[28][4];
+	char	aFree2[1688];
 } AVATAR_INFO;
+
+typedef struct
+{
+	int 	aType;
+	int 	aSort;
+	float 	aFrame;
+	float 	aLocation[3];
+	float 	aTargetLocation[3];
+	float 	aFront;
+	float 	aTargetFront;
+	int 	aTargetObjectSort;
+	int 	aTargetObjectIndex;
+	int 	aTargetObjectUniqueNumber;
+	int 	aSkillNumber;
+	int 	aSkillGradeNum1;
+	int 	aSkillGradeNum2;
+	int 	aSkillValue;
+} ACTION_INFO;
+
+typedef struct
+{
+	int     aVisibleState;
+	int     aSpecialState;
+	int		aKillOtherTribe;
+	int		aGuildMarkNum;
+	char	aGuildName[13];
+	char	aBuff0[3];
+	int     aGuildRole;
+	char	aCallName[5];
+	char	aBuff1[3];
+	int		aGuildMarkEffect;
+	char	aName[13];
+	char	aBuff2[3];
+	int     aTribe;
+	int     aPreviousTribe;
+	int     aGender;
+	int     aHeadType;
+	int     aFaceType;
+	int     aLevel1;
+	int     aLevel2;
+	int		aEquipForView[13][2];
+	int		aAnimalNumber;
+	int		aTitle;// 1-12 / 101-112 / 201-212 / 301-312 / 401-412
+	int		aHalo;	
+	int		aRebirth;
+	int		aUnknow1;
+	ACTION_INFO aAction;
+	int		aMaxLifeValue;
+	int		aLifeValue;
+	int		aMaxManaValue;
+	int		aManaValue;
+	int		aEffectValueForView[28];
+	char	aPartyName[13];
+	char	aBuff3[3];
+	int		aDuelState[3];
+	int     aPShopState;
+	char	aPShopName[25];
+	char	aBuff4[3];
+	int		aFashionNumber;
+	int		aGuildBuff[2];
+	int		aBotOn;
+	int		aUnknow3[5];
+	int		aRankPoint;
+	int		aArrow;
+	int		aAnimalOn;
+	int		aUnknow[7];
+} OBJECT_FOR_AVATAR;
 `
 // JS to binary // binary to JS
 global.pckAvatar = function( type, buf )
@@ -64,5 +131,22 @@ global.pckAvatar = function( type, buf )
 		a = context.AVATAR_INFO.toBinary( buf ); //pack array,object to buffer
 	return a;
 }
-global.AVATAR_INFO = context.AVATAR_INFO;
+global.pckAction = function( type, buf )
+{
+	var a;
+	if( type == 0 )
+		a = context.ACTION_INFO.fromBinary( buf );//unpack from buffer to array,object
+	if ( type == 1)
+		a = context.ACTION_INFO.toBinary( buf ); //pack array,object to buffer
+	return a;
+}
+global.pckObjectAvatar = function( type, buf )
+{
+	var a;
+	if( type == 0 )
+		a = context.OBJECT_FOR_AVATAR.fromBinary( buf );//unpack from buffer to array,object
+	if ( type == 1)
+		a = context.OBJECT_FOR_AVATAR.toBinary( buf ); //pack array,object to buffer
+	return a;
+}
 module.exports = global;
